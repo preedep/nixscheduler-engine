ba044645f5aae90b9cf8d5571b16c5ecd0e83041
Preedee Ponchevin <preedee.digital@gmail.com>
2025-04-17 10:04:08 +0700

    Add script for creating ADF pipeline job via API
    Introduce `curl_create_adf_job.sh` to send POST requests for scheduling ADF pipeline runs. The script sets up a cron-based job with payload details like subscription ID, resource group, factory name, pipeline, and parameters. This provides a streamlined way to automate ADF pipeline execution.

---
+++ curl_create_adf_job.sh
@@ -0,0 +1,16 @@
+curl -v -X POST http://localhost:8888/api/jobs \
+  -H "Content-Type: application/json" \
+  -d '{
+    "name": "Task-ADF",
+    "cron": "*/5 * * * *",
+    "task_type": "adf_pipeline",
+    "payload": "{
+      \"subscription_id\": \"9d3ff024-cfad-4108-a098-8e675fbc4cc4\",
+      \"resource_group\": \"RG-SG-NICKDEV001\",
+      \"factory_name\": \"MyNICKADF001\",
+      \"pipeline\": \"batch_account_java\",
+      \"parameters\": {
+        \"message\": \"Hello, World!\"
+      }
+    }"
+}'
